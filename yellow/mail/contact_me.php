5b2ed792dfc324e43284fb1b5612dfa34521cbe5
propastinv <Propastin.V@outlook.com>
2019-05-17 20:25:58 +0300

    Почта

--- yellow/mail/contact_me.php
+++ yellow/mail/contact_me.php
@@ -16,11 +16,11 @@ $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
    
 // Create the email and send the message
-$to = 'yourname@yourdomain.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";   
+$to = 'alcokod.info@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$email_subject = "Заявка от:  $name";
+$email_body = "Новая заявка.\n\n"."Детали:\n\nИмя: $name\n\nEmail: $email_address\n\nТелефон: $phone\n\Сообщение:\n$message";
+$headers = "От: alcokod.info@gmail.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers .= "Ответить: $email_address";   
 mail($to,$email_subject,$email_body,$headers);
 return true;         
 ?>
